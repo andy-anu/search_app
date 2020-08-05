11e4b3566b0d43ca416b76d7ce859e731a63dea7
andy-anu <8765anurag@gmail.com>
2020-08-06 00:55:57 +0530

    Updates

---
+++ precache-manifest.e1cc14e814dcb46ffa36f4e4f67a5c06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07270baa8c2d801af15cabbc0931ef99",
+    "url": "/search_app/index.html"
+  },
+  {
+    "revision": "6bb05f27fab0de24eea0",
+    "url": "/search_app/static/css/main.12a74b52.chunk.css"
+  },
+  {
+    "revision": "38860e334755485ce34d",
+    "url": "/search_app/static/js/2.40ccc4fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search_app/static/js/2.40ccc4fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bb05f27fab0de24eea0",
+    "url": "/search_app/static/js/main.7d8d9dd8.chunk.js"
+  },
+  {
+    "revision": "aa23120628940178fc6e",
+    "url": "/search_app/static/js/runtime-main.29b82250.js"
+  }
+]);
